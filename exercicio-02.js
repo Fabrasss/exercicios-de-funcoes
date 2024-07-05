eb00a29dc010420a6b203adfff1a7a4ede9597d6
FABRICIO ALMEIDA DIAS PINHEIRO <FABRICIO6174@DIGITALCOLLEGE.LOCAL>
2024-07-05 11:39:01 -0300

    exercicios

---
+++ exercicio-02.js
@@ -0,0 +1,5 @@
+function soma (num1, num2) {
+    return num1 + num2
+}
+let resultado = soma(3, 4)
+console.log(resultado)
---
+++ exercicio-03.js
@@ -0,0 +1,8 @@
+function fatorial(num1) {
+    let resultado = 1
+    for (let num1 = 0; num1 <= 0;  num1++)
+        resultado *= num1 ;
+        
+    
+}
+console.log(fatorial(3))
