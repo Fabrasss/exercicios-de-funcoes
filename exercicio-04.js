5ed79c14666597ca63b0fd15232f6aacf281edf3
Fabricio Almeida <fabas595653@gmail.com>
2024-07-14 17:05:44 -0300

    objeto

---
+++ exercicio-04.js
@@ -0,0 +1,5 @@
+function output(Text) {
+    console.log(Text);
+}
+
+output("gay");
---
+++ objeto.js
@@ -0,0 +1,8 @@
+let objeto = {
+    nome: {
+        primeiroNome: 'Fabras',
+        segundoNome: "Mohammed",
+    },
+    endereco: "josé"
+}
+console.log(objeto)
